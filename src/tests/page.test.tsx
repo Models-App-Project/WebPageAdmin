fdcfa76df95bca8035caa494f6c92e13c813a0b0
Daniel Silva <danielzcomputacao@gmail.com>
2024-09-27 00:02:59 -0300

    test(AuthPages): atualizar testes para os componentes das telas de Auth

--- src/tests/page.test.tsx
+++ src/tests/page.test.tsx
@@ -1,11 +1,27 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Page from "../app/page";
+import Login from "@/app/auth/login/page";
+import Register from "@/app/auth/register/page";
+import ResetPassword from "@/app/auth/resetpassword/page";
 
-describe("Page", () => {
-    it("renders a heading", () => {
-        render(<Page />);
-        const element = screen.getByText("Hello World"); // Exemplo, depende do seu componente
+describe("Login", () => {
+    it("renders a text in Login page", () => {
+        render(<Login />);
+        const element = screen.getByText("Login"); // Exemplo, depende do seu componente
+        expect(element).toBeInTheDocument();
+    });
+});
+describe("Register", () => {
+    it("renders a text in Register page", () => {
+        render(<Register />);
+        const element = screen.getByText("Register"); // Exemplo, depende do seu componente
+        expect(element).toBeInTheDocument();
+    });
+});
+describe("ResetPassword", () => {
+    it("renders a text in ResetPassword page", () => {
+        render(<ResetPassword />);
+        const element = screen.getByText("ResetPassword"); // Exemplo, depende do seu componente
         expect(element).toBeInTheDocument();
     });
 });
