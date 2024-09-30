bc51ebc27cf332b22ecfca43c7943592c6a1b337
Daniel Silva <danielzcomputacao@gmail.com>
2024-09-30 17:19:16 -0300

    fix(AuthPages) : Deletar rota de registro, pois o projet não vai contar com essa página

--- src/tests/page.test.tsx
+++ src/tests/page.test.tsx
@@ -1,27 +1,19 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Login from "@/app/auth/login/page";
-import Register from "@/app/auth/register/page";
 import ResetPassword from "@/app/auth/resetpassword/page";
 
 describe("Login", () => {
     it("renders a text in Login page", () => {
         render(<Login />);
-        const element = screen.getByText("Login"); // Exemplo, depende do seu componente
-        expect(element).toBeInTheDocument();
-    });
-});
-describe("Register", () => {
-    it("renders a text in Register page", () => {
-        render(<Register />);
-        const element = screen.getByText("Register"); // Exemplo, depende do seu componente
+        const element = screen.getByText("Login");
         expect(element).toBeInTheDocument();
     });
 });
 describe("ResetPassword", () => {
     it("renders a text in ResetPassword page", () => {
         render(<ResetPassword />);
-        const element = screen.getByText("ResetPassword"); // Exemplo, depende do seu componente
+        const element = screen.getByText("ResetPassword");
         expect(element).toBeInTheDocument();
     });
 });
